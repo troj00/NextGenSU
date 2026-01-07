8b2508976b01def9841b5f80cb3ba8c95bf11948
troj00 <124354827+troj00@users.noreply.github.com>
2026-01-07 21:39:10 +0200

    Update manager_sign.h

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -4,7 +4,7 @@
 // troj00/NextGenSU
 #define EXPECTED_SIZE_NEXTGENSU 0x2e8
 #define EXPECTED_HASH_NEXTGENSU                                                \
-    "746e4ea77ec6b9f1adb2cd5ece0ae3bca9dc8cb6620ddfce7984c3a976039064"
+    "56086aea5e32d56cceccd828c7665e37c448b6c0acbe513329ba3bbeb89034ca"
 
 typedef struct {
     u32 size;
