11d69d0cc376ffcf6e83e3a90b7f329ad0ad8dc0
troj00 <124354827+troj00@users.noreply.github.com>
2026-01-07 20:37:21 +0200

    test hash sign

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -4,7 +4,7 @@
 // troj00/NextGenSU
 #define EXPECTED_SIZE_NEXTGENSU 0x2e8
 #define EXPECTED_HASH_NEXTGENSU                                                \
-    "0f68387d37ffbc054e8427dcc37666a5df799dfcd971d841dd9a02c236db95f2"
+    "a3af1db3fc535c64e3b20ff2d9f9be8ebd89e2d81e19d10c7d044c3bc03b60af"
 
 typedef struct {
     u32 size;
