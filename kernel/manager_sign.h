c91571da2c3d1ad3f8e56bc3ffbb47414b48b8ed
troj00 <124354827+troj00@users.noreply.github.com>
2026-01-07 21:34:09 +0200

    Update manager_sign.h

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -3,7 +3,7 @@
 
 // NextGenSU/KernelSU
 #define EXPECTED_SIZE_NEXTGENSU   0x2e8
-#define EXPECTED_HASH_NEXTGENSU   "a3af1db3fc535c64e3b20ff2d9f9be8ebd89e2d81e19d10c7d044c3bc03b60af"
+#define EXPECTED_HASH_NEXTGENSU   "746e4ea77ec6b9f1adb2cd5ece0ae3bca9dc8cb6620ddfce7984c3a976039064"
 
 // weishu/KernelSU
 #define EXPECTED_SIZE_WEISHU   0x033b
